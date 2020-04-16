0048dce13e0438ce5a462c01359e7594b9a5aa02
alkapov <ralkapov@gmail.com>
2020-04-17 01:55:27 +0300

    add test from part e

--- week-1/transport-catalog-part-g/tests.cpp
+++ week-1/transport-catalog-part-g/tests.cpp
@@ -1,7 +1,10 @@
 #include "tests.h"
 
+#include "json.h"
+#include "requests.h"
 #include "svg.h"
 #include "test_runner.h"
+#include "transport_catalog.h"
 
 namespace {
 
@@ -19,7 +22,174 @@ constexpr std::string_view kCourseraExampleSvg =
     "</text>"
     "</svg>";
 
-void SimpleTest() {
+constexpr std::string_view kPartEFirstRequest = R"(
+{
+  "routing_settings": {
+    "bus_wait_time": 6,
+    "bus_velocity": 40
+  },
+  "base_requests": [
+    {
+      "type": "Bus",
+      "name": "297",
+      "stops": [
+        "Biryulyovo Zapadnoye",
+        "Biryulyovo Tovarnaya",
+        "Universam",
+        "Biryulyovo Zapadnoye"
+      ],
+      "is_roundtrip": true
+    },
+    {
+      "type": "Bus",
+      "name": "635",
+      "stops": [
+        "Biryulyovo Tovarnaya",
+        "Universam",
+        "Prazhskaya"
+      ],
+      "is_roundtrip": false
+    },
+    {
+      "type": "Stop",
+      "road_distances": {
+        "Biryulyovo Tovarnaya": 2600
+      },
+      "longitude": 37.6517,
+      "name": "Biryulyovo Zapadnoye",
+      "latitude": 55.574371
+    },
+    {
+      "type": "Stop",
+      "road_distances": {
+        "Prazhskaya": 4650,
+        "Biryulyovo Tovarnaya": 1380,
+        "Biryulyovo Zapadnoye": 2500
+      },
+      "longitude": 37.645687,
+      "name": "Universam",
+      "latitude": 55.587655
+    },
+    {
+      "type": "Stop",
+      "road_distances": {
+        "Universam": 890
+      },
+      "longitude": 37.653656,
+      "name": "Biryulyovo Tovarnaya",
+      "latitude": 55.592028
+    },
+    {
+      "type": "Stop",
+      "road_distances": {},
+      "longitude": 37.603938,
+      "name": "Prazhskaya",
+      "latitude": 55.611717
+    }
+  ],
+  "stat_requests": [
+    {
+      "type": "Bus",
+      "name": "297",
+      "id": 1
+    },
+    {
+      "type": "Bus",
+      "name": "635",
+      "id": 2
+    },
+    {
+      "type": "Stop",
+      "name": "Universam",
+      "id": 3
+    },
+    {
+      "type": "Route",
+      "from": "Biryulyovo Zapadnoye",
+      "to": "Universam",
+      "id": 4
+    },
+    {
+      "type": "Route",
+      "from": "Biryulyovo Zapadnoye",
+      "to": "Prazhskaya",
+      "id": 5
+    }
+  ]
+}
+)";
+constexpr std::string_view kPartEFirstResponse = R"(
+[
+    {
+        "curvature": 1.42963,
+        "unique_stop_count": 3,
+        "stop_count": 4,
+        "request_id": 1,
+        "route_length": 5990
+    },
+    {
+        "curvature": 1.30156,
+        "unique_stop_count": 3,
+        "stop_count": 5,
+        "request_id": 2,
+        "route_length": 11570
+    },
+    {
+        "request_id": 3,
+        "buses": [
+            "297",
+            "635"
+        ]
+    },
+    {
+        "total_time": 11.235,
+        "items": [
+            {
+                "time": 6,
+                "type": "Wait",
+                "stop_name": "Biryulyovo Zapadnoye"
+            },
+            {
+                "span_count": 2,
+                "bus": "297",
+                "type": "Bus",
+                "time": 5.235
+            }
+        ],
+        "request_id": 4
+    },
+    {
+        "total_time": 24.21,
+        "items": [
+            {
+                "time": 6,
+                "type": "Wait",
+                "stop_name": "Biryulyovo Zapadnoye"
+            },
+            {
+                "span_count": 2,
+                "bus": "297",
+                "type": "Bus",
+                "time": 5.235
+            },
+            {
+                "time": 6,
+                "type": "Wait",
+                "stop_name": "Universam"
+            },
+            {
+                "span_count": 1,
+                "bus": "635",
+                "type": "Bus",
+                "time": 6.975
+            }
+        ],
+        "request_id": 5
+    }
+]
+)";
+
+void SimpleSvgTest() {
   auto circle = Svg::Circle().SetCenter({.x = -2, .y = 10});
   auto doc = Svg::Document{};
   doc.Add(circle);
@@ -30,7 +200,7 @@ void SimpleTest() {
       stream.str());
 }
 
-void CourseraExample() {
+void CourseraSvgExample() {
   Svg::Document svg;
 
   svg.Add(Svg::Polyline{}
@@ -64,10 +234,41 @@ void CourseraExample() {
   ASSERT_EQUAL(stream.str(), kCourseraExampleSvg);
 }
 
+void MakeRequest(std::istream& input, std::ostream& output) {
+  const auto input_doc = Json::Load(input);
+  const auto& input_map = input_doc.GetRoot().AsMap();
+
+  const TransportCatalog db(
+      Descriptions::ReadDescriptions(input_map.at("base_requests").AsArray()),
+      input_map.at("routing_settings").AsMap());
+
+  Json::PrintValue(
+      Requests::ProcessAll(db, input_map.at("stat_requests").AsArray()),
+      output);
+}
+
+std::stringstream MakeExpectedFromJson(const std::string_view view) {
+  std::stringstream input{view.data()};
+  std::stringstream output{};
+  Json::Print(Json::Load(input), output);
+  return output;
+}
+
+void CourseraPartEFirstCase() {
+  std::stringstream input{kPartEFirstRequest.data()};
+  std::stringstream output{};
+
+  const std::stringstream expected = MakeExpectedFromJson(kPartEFirstResponse);
+
+  MakeRequest(input, output);
+  ASSERT_EQUAL(output.str(), expected.str());
+}
+
 }  // namespace
 
 void RunTests() {
   TestRunner tr;
-  RUN_TEST(tr, SimpleTest);
-  RUN_TEST(tr, CourseraExample);
+  RUN_TEST(tr, SimpleSvgTest);
+  RUN_TEST(tr, CourseraSvgExample);
+  RUN_TEST(tr, CourseraPartEFirstCase);
 }
